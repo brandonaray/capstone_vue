f9778358998db86d9e75c0386601541efb42a9b8
brandonaray <brandonray1225@gmail.com>
2019-04-10 20:03:04 -0500

    add song_versions index

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Home from "./views/Home.vue";
 import Signup from "./views/Signup.vue";
 import Login from "./views/Login.vue";
 import Logout from "./views/Logout.vue";
+import SongVersionsIndex from "./views/SongVersionsIndex.vue";
 
 Vue.use(Router);
 
@@ -38,6 +39,11 @@ export default new Router({
       path: "/logout",
       name: "logout",
       component: Logout
+    },
+    {
+      path: "/songs",
+      name: "songs",
+      component: SongVersionsIndex
     }
   ]
 });
---
+++ src/views/SongVersionsIndex.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="continer">
+    <h1>All Songs</h1>
+    <div v-for="song_version in song_versions">
+      {{ song_version.title }}
+      {{ song_version.artist }}
+      {{ song_version.label }}
+      {{ song_version.duration }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      song_versions: []
+    };
+  },
+  created: function() {
+    axios.get("api/song_versions").then(response => {
+      this.song_versions = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
