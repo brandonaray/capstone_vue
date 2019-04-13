8694ba7084a46836844c00dd21e57cabc90c51aa
brandonaray <brandonray1225@gmail.com>
2019-04-13 15:26:27 -0500

    add vue2 filters to songs index vue

--- src/views/SongVersionsIndex.vue
+++ src/views/SongVersionsIndex.vue
@@ -23,11 +23,14 @@
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
-      song_versions: []
+      song_versions: [],
+      searchFilter: ""
     };
   },
   created: function() {
