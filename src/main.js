ae37753c248945a7df20ae3a15ae91d74d1dc6ea
brandonaray <brandonray1225@gmail.com>
2019-04-30 19:03:19 -0500

    install vue instant library

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import "vue-instant/dist/vue-instant.css";
+import VueInstant from "vue-instant";
+Vue.use(VueInstant);
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
