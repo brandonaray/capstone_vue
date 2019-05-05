1258addbe2429308d803e6311545f8b44a3d6872
brandonaray <brandonray1225@gmail.com>
2019-05-04 20:16:12 -0500

    move song list styling to App.vue

--- src/views/SongVersionsIndex.vue
+++ src/views/SongVersionsIndex.vue
@@ -47,7 +47,7 @@
             <div class="col-md-3 list-el">{{ song_version.artist }}</div>
             <div class="col-md-2 list-el">{{ song_version.friendly_duration }}</div>
             <div class="col-md-2">
-              <button class="add-queue">
+              <button class="add-queue" v-on:click="addToQueue(song_version)">
                 Add to Queue
               </button>
             </div>
@@ -100,91 +100,3 @@ export default {
   }
 };
 </script>
-<style>
-.content {
-  width: 1140px;
-  margin: 0px 150px;
-}
-.content h2 {
-  font-family: Bungee;
-  font-size: 42px;
-  letter-spacing: 1.1px;
-  color: #f5fefe;
-  height: 63px;
-  width: 703px;
-  display: inline-block;
-}
-.form-wrapper {
-  height: 63px;
-  width: 420px;
-  float: right;
-  text-align: right;
-}
-.song-list {
-  width: 1140px;
-  height: 545px;
-  border: solid 1px #4a42bf;
-  background-color: #050424;
-  overflow: scroll;
-}
-#title-bar {
-  background-color: #56ffcf;
-  height: 52px;
-  width: 1138px;
-  margin: 0;
-  position: fixed;
-  z-index: 1000;
-}
-.bar-el {
-  padding: 17px 0px 16px 31px;
-}
-h5 {
-  font-family: Muli;
-  font-size: 18px;
-  font-weight: bold;
-  color: #2b2672;
-  user-select: none;
-}
-.list-wrapper {
-  margin-top: 53px;
-}
-#songs {
-  border: solid #4a42bf 1px;
-  max-width: 1138px;
-  height: 62px;
-  margin: 0px;
-}
-#songs:hover {
-  background-color: #1c184a;
-}
-.list-el {
-  font-family: Muli;
-  font-size: 17px;
-  color: #f5fefe;
-  padding: 17px 0px 16px 31px;
-  user-select: none;
-}
-.add-queue {
-  width: 151px;
-  height: 36px;
-  border-radius: 4px;
-  border-style: solid;
-  border-width: 1px;
-  border-image-source: linear-gradient(to right, #ff6602, #f42ea0);
-  border-image-slice: 1;
-  background-color: #090819;
-  font-family: Bungee;
-  font-size: 15px;
-  color: #f5fefe;
-  margin: 12px 27px 14px 0px;
-}
-.add-queue:hover {
-  box-shadow: 0 0 8px 0 #ff3cac;
-}
-.add-queue:focus {
-  outline: none;
-}
-.add-queue:active {
-  border-image-source: linear-gradient(to left, #ff6602, #f42ea0);
-}
-</style>
